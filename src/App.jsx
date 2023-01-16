3c7c09f2eac202f817046c6589a6103af85464e6
JYROH <ghgt1@naver.com>
2023-01-16 16:25:45 +0900

    feat: Import Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-
   return (
     <div className="App">
-      
+      <Header />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
