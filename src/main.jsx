0549f4fd513d99c9df15c33f0e2e50aed5ad62c0
plou102 <plou102@naver.com>
2023-01-16 15:34:29 +0900

    update router

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import PlayPage from './pages/PlayPage/PlayPage'
+import SearchPage from './pages/SearchPage/SearchPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/:value',
+        element: <SearchPage />
+      },
+      {
+        path: '/:id',
+        element: <PlayPage />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <RouterProvider router={router} />
 )
