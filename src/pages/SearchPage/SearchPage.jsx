f377c6cd186740e2555670878dc45d77a0e4155c
plou102 <plou102@naver.com>
2023-01-17 14:25:33 +0900

    test api response

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 export default function SearchPage () {
+  const [search, setSearch] = useState([]);
+
+  const params = {
+    part: 'snippet',
+    q: 'kakao',
+    maxResults: 2,
+  }
+
+  useEffect(() => {
+    async function getData() {
+      // const data = await getSearch(params);
+      setSearch(data);
+    }
+    getData();
+  }, []);
+
+  console.log(search);
+  console.log(123);
   return (
     <div>
       <p>search</p>
