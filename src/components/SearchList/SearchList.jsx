42e51fdd4b4e43a40c7b5ef3200954732ba46b3c
plou102 <plou102@naver.com>
2023-01-18 12:42:00 +0900

    edit SubNavBar && update SearchPage

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import styles from './SearchList.module.scss'
 
 export default function SearchList({data}) {
-  let imgUrl = data.snippet.thumbnails.default.url;
+  const [channel, setChannel] = useState([]);
+
+  let channelId = data?.snippet.channelId;
+  const params = {
+    part: 'snippet',
+    id: channelId,
+    maxResults: 1,
+    key: import.meta.env.VITE_API_KEY,
+  }
+
+  useEffect(() => {
+    async function getData() {
+      const data = await axios.get('https://www.googleapis.com/youtube/v3/channels', { params });
+      setChannel(data.data.items);
+    }
+    getData();
+  }, []);
+
+  console.log(channel);
+
+  let videoThumbnail = data?.snippet.thumbnails.medium.url;
+  let channelThumbnail = channel?.snippet.thumbnails.default.url;
+
   return (
     <div className={styles.videoContent}>
       <div className={styles.imgContent}>
-      <img src={imgUrl} alt='video thumbnail' className={styles.thumbnail}></img>
+        <img src={videoThumbnail} alt='video thumbnail' className={styles.thumbnail}></img>
       </div>
       <div className={styles.textContent}>
-        <h2>{data.snippet.title}</h2>
-        {/* 예시 */}
-        <p>조회수 20회</p>
-        <p>3개월 전</p>
-        <p>{data.snippet.channelTitle}</p>
+        <h3>{data.snippet.title}</h3>
+        {/* 조회수랑 기간은 예시로 적어 놓았습니다~~ */}
+        <span>조회수 20회 <span className={styles.dot_separator}> • </span> 3개월 전</span>
+        <img src={channelThumbnail} alt='channel thumbnail' className={styles.channelThumbnail}></img>
+        <p className={styles.channelTitle}>{data.snippet.channelTitle}</p>
         <p>{data.snippet.description}</p>
       </div>
     </div>
