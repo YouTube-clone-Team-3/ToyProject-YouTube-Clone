4c6ed487ace26709e00eee8be941f928e944f0b2
JYROH <ghgt1@naver.com>
2023-01-18 15:16:21 +0900

    fix: Fix Number base to Korean base

--- src/utils/CalNum.js
+++ src/utils/CalNum.js
@@ -1,15 +1,17 @@
 const calcNum = (num) => {
   if (num >= 0 && num < 1000) {
-    return num
-  } else if (num >= 1000 && num < 1000000) {
-    const calcK = num.split('').reverse().slice(2).reverse()
-    calcK.splice(calcK.length - 1, 0, '.')
-    return `${calcK.join('')}K`
-  } else if (num >= 1000000) {
-    const calcM = num.split('').reverse().slice(5).reverse()
-    calcM.splice(calcM.length - 1, 0, '.')
-    return `${calcM.join('')}M`
+    return num;
+  } else if (num >= 1000 && num < 10000) {
+    return `${(num / 1000).toFixed(1)}천`;
+  } else if (num < 100000) {
+    return `${(num / 10000).toFixed(1)}만`;
+  } else if (num < 100000000) {
+    return `${Math.floor(num / 10000)}만`;
+  } else if (num < 1000000000) {
+    return `${(num / 100000000).toFixed(1)}억`;
+  } else {
+    return `${Math.floor(num / 100000000)}억`;
   }
-}
+};
 
-export default calcNum
+export default calcNum;
