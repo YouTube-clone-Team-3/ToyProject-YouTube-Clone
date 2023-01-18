95980cdc41838b287fef857989f4b24fbf371bc6
JYROH <ghgt1@naver.com>
2023-01-18 19:44:58 +0900

    feat: Add VideoCard Hover action

--- src/components/RelatedVideos/EachRelatedVideo.jsx
+++ src/components/RelatedVideos/EachRelatedVideo.jsx
@@ -41,7 +41,6 @@ const EachRelatedVideo = ({ item, index }) => {
   }, []);
 
   const video = item.snippet;
-  console.log(relatedVideoSearch);
   const videoDuration =
     relatedVideoSearch[index]?.data?.items[0].contentDetails.duration;
   const videoViews =
@@ -58,6 +57,26 @@ const EachRelatedVideo = ({ item, index }) => {
             <img src={video.thumbnails.medium.url} alt={video.title} />
             <div className={styles.durationBox}>
               <span>{duration}</span>
+            </div>
+            <div className={styles.hoverBox}>
+              <svg viewBox="0 0 24 24">
+                <g>
+                  <path
+                    fill="#ffffff"
+                    d="M14.97,16.95L10,13.87V7h2v5.76l4.03,2.49L14.97,16.95z M12,3c-4.96,0-9,4.04-9,9s4.04,9,9,9s9-4.04,9-9S16.96,3,12,3 M12,2c5.52,0,10,4.48,10,10s-4.48,10-10,10S2,17.52,2,12S6.48,2,12,2L12,2z"
+                  ></path>
+                </g>
+              </svg>
+            </div>
+            <div className={styles.hoverBox}>
+              <svg viewBox="0 0 24 24">
+                <g>
+                  <path
+                    fill="#ffffff"
+                    d="M21,16h-7v-1h7V16z M21,11H9v1h12V11z M21,7H3v1h18V7z M10,15l-7-4v8L10,15z"
+                  ></path>
+                </g>
+              </svg>
             </div>
           </div>
           <div className={styles.description}>
