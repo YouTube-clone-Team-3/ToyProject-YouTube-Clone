967377930445ae6da9b6e9ffd899984f0933cb69
quokka-eating-carrots <ovzip7@gmail.com>
2023-01-19 21:51:19 +0900

    Feat: video auto play

--- src/App.jsx
+++ src/App.jsx
@@ -26,18 +26,18 @@ function App() {
   });
 
   function displayClass() {
-    if (isPc && display) {
-      return "big"
+    if (isPc) {
+      return display ? "big" : "small"
     } else {
-      return "small"
+      return display ? "small" : "big"
     }
   }
 
   function displayNav() {
-    if (isPc && display) {
-      return <NavBar />
+    if (isPc) {
+      return display ? <NavBar /> : <SubNavBar />
     } else {
-      return <SubNavBar />
+      return display ? <SubNavBar /> : <NavBar />
     }
   }
 
