37d7bee373c3da2f03f4ab0e42cecf4e0cefe7e2
JYROH <ghgt1@naver.com>
2023-01-20 15:00:10 +0900

    feat: Edit Thumbnail images to high

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -1,41 +1,39 @@
-import axios from "axios"
-import React, { useEffect, useState } from "react"
-import styles from './SearchList.module.scss'
-import calcNum from "../../utils/CalNum"
-import calcDate from "../../utils/CalDate"
-import calcDuration from "../../utils/CalDuration"
-import channelData from '../../data/channel.json'
-import videoData from '../../data/video.json'
-import { useNavigate } from "react-router-dom"
-import { FiMoreVertical } from 'react-icons/fi'
-import { HiOutlineClock } from 'react-icons/hi'
-import { RiPlayList2Fill } from 'react-icons/ri'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import styles from "./SearchList.module.scss";
+import calcNum from "../../utils/CalNum";
+import calcDate from "../../utils/CalDate";
+import calcDuration from "../../utils/CalDuration";
+import channelData from "../../data/channel.json";
+import videoData from "../../data/video.json";
+import { useNavigate } from "react-router-dom";
+import { FiMoreVertical } from "react-icons/fi";
+import { HiOutlineClock } from "react-icons/hi";
+import { RiPlayList2Fill } from "react-icons/ri";
 
-export default function SearchList({data}) {
+export default function SearchList({ data }) {
   const [channel, setChannel] = useState([]);
   const [video, setVideo] = useState([]);
   const navigate = useNavigate();
 
   let channelId = data?.snippet.channelId;
   const params = {
-    part: 'snippet',
+    part: "snippet",
     id: channelId,
     maxResults: 1,
     key: import.meta.env.VITE_API_KEY,
-  }
+  };
 
   useEffect(() => {
     async function getData() {
       // const data = await axios.get('https://www.googleapis.com/youtube/v3/channels', { params });
       setChannel(channelData.items);
       setVideo(videoData.items);
-
     }
     getData();
   }, []);
 
-
-  const videoThumbnail = data?.snippet.thumbnails.medium.url;
+  const videoThumbnail = data?.snippet.thumbnails.high.url;
   const channelThumbnail = channel[0]?.snippet.thumbnails.default.url;
 
   const viewCount = calcNum(video[0]?.statistics.viewCount);
@@ -44,26 +42,44 @@ export default function SearchList({data}) {
 
   return (
     <div className={styles.videoContent}>
-      <FiMoreVertical size='24' className={styles.moreIcon} />
+      <FiMoreVertical size="24" className={styles.moreIcon} />
       <div className={styles.imgContent}>
-        <HiOutlineClock size='22' className={styles.clockIcon} />
-        <RiPlayList2Fill size='21' className={styles.playlistIcon} />
-        <img src={videoThumbnail} alt='video thumbnail' className={styles.thumbnail} onClick={() => {
-          navigate(`/detail/${video[0]?.id}`);
-        }} ></img>
+        <HiOutlineClock size="22" className={styles.clockIcon} />
+        <RiPlayList2Fill size="21" className={styles.playlistIcon} />
+        <img
+          src={videoThumbnail}
+          alt="video thumbnail"
+          className={styles.thumbnail}
+          onClick={() => {
+            navigate(`/detail/${video[0]?.id}`);
+          }}
+        ></img>
         <p className={styles.duration}>{duration}</p>
       </div>
       <div className={styles.textContent}>
-        <h3 onClick={() =>{
-          navigate(`/detail/${video[0]?.id}`);
-        }}>{data.snippet.title}</h3>
-        <span>조회수 {viewCount}회 <span className={styles.dot_separator}> • </span> {publishedAt} 전</span>
+        <h3
+          onClick={() => {
+            navigate(`/detail/${video[0]?.id}`);
+          }}
+        >
+          {data.snippet.title}
+        </h3>
+        <span>
+          조회수 {viewCount}회 <span className={styles.dot_separator}> • </span>{" "}
+          {publishedAt} 전
+        </span>
         <div className={styles.channelInfo}>
-          <img src={channelThumbnail} alt='channel thumbnail' className={styles.channelThumbnail}></img>
-          <span className={styles.channelTitle}>{data.snippet.channelTitle}</span>
+          <img
+            src={channelThumbnail}
+            alt="channel thumbnail"
+            className={styles.channelThumbnail}
+          ></img>
+          <span className={styles.channelTitle}>
+            {data.snippet.channelTitle}
+          </span>
         </div>
         <p>{data.snippet.description}</p>
       </div>
     </div>
-  )
+  );
 }
