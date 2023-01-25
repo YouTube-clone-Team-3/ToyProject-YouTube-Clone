163c5d613ddfcbed5e9826f9d1f75928ab3119b4
ParkJeongMin <107393773+plou102@users.noreply.github.com>
2023-01-25 16:54:25 +0900

    delete console.log

--- src/pages/PlayPage/RelatedVideo/index.jsx
+++ src/pages/PlayPage/RelatedVideo/index.jsx
@@ -6,7 +6,6 @@ import axios from "axios";
 const RelatedVideos = ({ id }) => {
   // 원래는 url params로 video id 가져와서 datas에 넣기. 그러면 아래는 똑같이 동작.
   // 테스트때는 index를 props로 넘겨주어 relatedVideoSearch.json 목업에서 순서대로 불러오겠음. 추후 지우기@@@@
-  console.log(id);
   const [relatedVideos, setRelatedVideos] = useState("");
 
   // 실제 api
